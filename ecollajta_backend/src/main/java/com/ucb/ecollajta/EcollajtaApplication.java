93bed35acc6d713bad9bfdf6b50cc7753fc0542b
Matheo Leonardo Quiroga Sanchez <matheo.quiroga@ucb.edu.bo>
2026-05-16 11:01:43 -0400

    init: Inicializacion del backend spring

---
+++ ecollajta_backend/src/main/java/com/ucb/ecollajta/EcollajtaApplication.java
@@ -0,0 +1,13 @@
+package com.ucb.ecollajta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcollajtaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcollajtaApplication.class, args);
+	}
+
+}
---
+++ ecollajta_backend/src/test/java/com/ucb/ecollajta/EcollajtaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucb.ecollajta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcollajtaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
